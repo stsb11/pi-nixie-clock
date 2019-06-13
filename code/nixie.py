4a82f35e44a17658d14901c3f1ea77b3bb46d25a
stsb11 <stephen.brown@bourne-grammar.lincs.sch.uk>
2019-06-13 20:21:05 +0100

    Added PWM dots and BBC weather

--- code/nixie.py
+++ code/nixie.py
@@ -1,8 +1,13 @@
-# Nixie GPS-powered clock, v0.1
+# Nixie Pi clock, v0.1
 import datetime, random, time
 import RPi.GPIO as GPIO
+import feedparser
+
 GPIO.setmode(GPIO.BCM) # I.e. the 5V pin in the top-left corner is referred to as 2.
 GPIO.setwarnings(False)
+
+bbcWeather = "2643029"
+maxDotBright = 60   # Maximum brightness (100max) of INS-1 dots.
 
 # Wiring below, to K155ID1 IC ABCD inputs (A is LSB, D is MSB).
 # Each sub-list goes from MSB to LSB (D to A). 
@@ -45,10 +50,31 @@ for lamp in lamps:
 
 GPIO.setup(bDots, GPIO.OUT)
 GPIO.setup(tDots, GPIO.OUT)
+pbDots = GPIO.PWM(bDots, 50)
+ptDots = GPIO.PWM(tDots, 50)
 
 # ****************************
 # Functions start here
 # ********************
+
+def weather(areaCode):
+    url = "https://weather-broker-cdn.api.bbci.co.uk/en/observation/rss/" + areaCode
+
+    NewsFeed = feedparser.parse(url)
+    entry = NewsFeed.entries[0]
+    weatherString = entry.description
+
+    tempStart = weatherString.find(" ") + 1
+    tempEnd = weatherString.find("C") - 1
+    temperature = int(weatherString[tempStart:tempEnd])
+
+    humidStart = weatherString.find("Humidity: ") + 9
+    humidEnd = weatherString.find("%")
+    humidity = int(weatherString[humidStart:humidEnd])
+    print("Temp: " + str(temperature) + ". H:" + str(humidity) + ".")
+    showOutput(humidity, -1, abs(temperature))
+    
+    
 def showOutput(hrs, mins, secs):
     # Function to illuminate tubes
     # Pass in three pairs of values to appear.
@@ -66,23 +92,22 @@ def showOutput(hrs, mins, secs):
             lightUp(currDigit, num // 10)
 	    lightUp(currDigit + 1, num - ((num // 10) * 10))
 
-            global tDots
-            global bDots
-            GPIO.output(tDots, 0)
-            GPIO.output(bDots, 1)
-        else:
+            global ptDots, pbDots
+            global maxDotBright
+            pbDots.start(maxDotBright)
+            ptDots.start(0)
+        elif item >= 0:
 	    lightUp(currDigit, 0)
 	    lightUp(currDigit + 1, item)
+        else:
+            # If we're here, we've been sent something like -1.
+            lightUp(currDigit, -1)
+	    lightUp(currDigit + 1, -1)
 
         currDigit = currDigit + 2
 
 def slideOff(hrs, mins, secs):
     # Makes current time 'slide off' the display.
-    global tDots
-    global bDots
-    GPIO.output(tDots, 0)
-    GPIO.output(bDots, 0)
-
     timeArr = [hrs, mins, secs]
     y = 6
     for x in range(1, -7, -1):
@@ -138,10 +163,6 @@ def slideOff(hrs, mins, secs):
 def cycleNums():
     # Get each tube to show 10 different digits at (pseudo)random, over the space of 1sec.
     print("Cathode protection cycle starting...")
-    global tDots
-    global bDots
-    GPIO.output(tDots, 0)
-    GPIO.output(bDots, 0)
     
     for x in range(20):
         for y in range(1, 7):
@@ -154,15 +175,11 @@ def cycleNums():
 def cycleNums2():
     # Get each tube to show 10 different digits at (pseudo)random, over the space of 1sec.
     print("Cathode protection cycle starting...")
-    global tDots
-    global bDots
-    GPIO.output(tDots, 0)
-    GPIO.output(bDots, 0)
 
     now = datetime.datetime.now()
     hrs = now.hour
     mins = now.minute
-    secs = now.second
+    secs = now.second + 1  # It takes about a second to do the animation.
     timeArr = [hrs, mins, secs]
     
     startAt = 1
@@ -197,17 +214,26 @@ def lightUp(lamp, numToShow):
     # Showing a digit where the value is >9 turns it off, when using the K155ID1 IC.
     if lamp > 0 and lamp < 7:
         lamp = lamp - 1
-        binNum = str(bin(numToShow))[2:]   # binNum will be something like '10'
 
-        while len(binNum) < 4:  # Add leading zeroes to get it to 4 bits.
-            binNum = '0' + binNum 
+        if numToShow >= 0:
+            binNum = str(bin(numToShow))[2:]   # binNum will be something like '10'
+            #print("numToShow is " + str(numToShow) + " on lamp " + str(lamp) + ".")
+
+            while len(binNum) < 4:  # Add leading zeroes to get it to 4 bits.
+                binNum = '0' + binNum 
         
-        GPIO.output(lamps[lamp][0], int(binNum[0]))
-        GPIO.output(lamps[lamp][1], int(binNum[1]))
-        GPIO.output(lamps[lamp][2], int(binNum[2]))
-        GPIO.output(lamps[lamp][3], int(binNum[3]))    
-        # print("Lamp " + str(lamp + 1) + " showing " + str(numToShow))
-
+            GPIO.output(lamps[lamp][0], int(binNum[0]))
+            GPIO.output(lamps[lamp][1], int(binNum[1]))
+            GPIO.output(lamps[lamp][2], int(binNum[2]))
+            GPIO.output(lamps[lamp][3], int(binNum[3]))    
+            # print("Lamp " + str(lamp + 1) + " showing " + str(numToShow))
+        else:
+            GPIO.output(lamps[lamp][0], 1)
+            GPIO.output(lamps[lamp][1], 1)
+            GPIO.output(lamps[lamp][2], 1)
+            GPIO.output(lamps[lamp][3], 1)
+            # Turn the lamp in question off if <0 is received.
+            
 
 # *********************************
 # Main loop starts here
@@ -233,23 +259,33 @@ while True:
 
     if secs==15:
         # Cycle the tubes through different digits to prevent cathode poisoning.
+        pbDots.start(0)
+        ptDots.start(0)
         slideOff(hrs, mins, secs)
     elif secs==20:
+        pbDots.start(0)
+        ptDots.start(0)
         cycleNums2()
     elif secs >= 16 and secs <= 20: # or (secs >= 46 and secs <= 50)):
         # Show the date at quarter past each minute for 5s
+        #pbDots.start(0)
+        #ptDots.start(0)
 	showOutput (day, month, year)
+    elif secs==45 or secs == 51:
+        cycleNums()
+    elif secs > 45 and secs <=50:
+        # Show the local humidity (left) and temperature (right)
+        weather(bbcWeather)
     else:
         # Show the time on the tubes.
 	showOutput(hrs, mins, secs)
-        # Deal with the dots.
-        global tDots
-        global bDots
+        # Deal with the dots...
+        duty = int((milli / 999999) * maxDotBright)
         if milli < 500000:
-            GPIO.output(tDots, 1)
-            GPIO.output(bDots, 1)
+            pbDots.start(duty)
+            ptDots.start(duty)
         else:
-            GPIO.output(tDots, 0)
-            GPIO.output(bDots, 0)
+            pbDots.start(maxDotBright - duty)
+            ptDots.start(maxDotBright - duty)
 
-    time.sleep(0.1)
+    time.sleep(0.05)
